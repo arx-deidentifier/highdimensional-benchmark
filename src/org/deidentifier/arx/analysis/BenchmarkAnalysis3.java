33be7f7138a3573406a55389fdc912b05fea2715
Fabian Prasser <mail@fabian-prasser.de>
2015-07-08 16:33:04 +0200

    Add analysis

--- src/org/deidentifier/arx/analysis/BenchmarkAnalysis3.java
+++ src/org/deidentifier/arx/analysis/BenchmarkAnalysis3.java
@@ -1,3 +1,22 @@
+/*
+ * Source code of the experiments from our 2015 paper 
+ * "Utility-driven anonymization of high-dimensional data"
+ *      
+ * Copyright (C) 2015 Fabian Prasser, Raffael Bild, Johanna Eicher, Helmut Spengler, Florian Kohlmayer
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.deidentifier.arx.analysis;
 
 import java.io.File;
@@ -29,7 +48,19 @@ import de.linearbits.subframe.render.GnuPlotParams.KeyPos;
 import de.linearbits.subframe.render.LaTeX;
 import de.linearbits.subframe.render.PlotGroup;
 
+
+/**
+ * Analysis of the results of the 3rd set of experiments
+ * @author Fabian Prasser
+ */
 public class BenchmarkAnalysis3 {
+    
+    /**
+     * Main
+     * @param args
+     * @throws IOException
+     * @throws ParseException
+     */
     public static void main(String[] args) throws IOException, ParseException {
         
         // Prepare
