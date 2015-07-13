43497e1fe26bede7ae936515dd7a4d1cf7a69468
Fabian Prasser <mail@fabian-prasser.de>
2015-07-13 10:54:21 +0200

    Update binaries and results

--- src/org/deidentifier/arx/algorithm/AlgorithmLightning.java
+++ src/org/deidentifier/arx/algorithm/AlgorithmLightning.java
@@ -144,7 +144,7 @@ public class AlgorithmLightning extends BenchmarkAlgorithm{
         // Depending on monotony of metric we choose to compare either IL or monotonic subset with the global optimum
         boolean prune = false;
         if (getGlobalOptimum() != null) {
-            if (metricMonotonic) prune = transformation.getInformationLoss().compareTo(getGlobalOptimum().getInformationLoss()) >= 0;
+            if (metricMonotonic) prune = transformation.getLowerBound().compareTo(getGlobalOptimum().getInformationLoss()) >= 0;
         }
         return (prune || transformation.hasProperty(propertyExpanded));
     }
